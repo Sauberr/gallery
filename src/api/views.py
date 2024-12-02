0fc08d0810da33f74a796f9ff2f6f6d033593931
Toma <greebsasha24@gmail.com>
2024-12-02 20:46:44 +0300

    Add: viewset for subscriptions, basic, premium, enterprise and images.

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from subscriptions.models import Basic, Enterprise, Premium, Subscription
+from subscriptions.serializers import BasicSubscriptionSerializer, EnterpriseSubscriptionSerializer,  \
+    PremiumSubscriptionSerializer, SubscriptionSerializer
+from images.models import Images
+from images.serializers import ImagesSerializer
+from rest_framework.permissions import IsAuthenticated
+from api.permissions import IsAdminOrReadOnly
+from rest_framework import filters
 
-# Create your views here.
+
+class SubscriptionViewSet(ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+
+class BasicSubscriptionViewSet(ModelViewSet):
+    serializer_class = BasicSubscriptionSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        return Basic.objects.filter(id=1)
+
+
+class PremiumSubscriptionViewSet(ModelViewSet):
+    serializer_class = PremiumSubscriptionSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        return Premium.objects.filter(id=1)
+
+
+class EnterpriseSubscriptionViewSet(ModelViewSet):
+    serializer_class = EnterpriseSubscriptionSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        return Enterprise.objects.filter(id=1)
+
+
+class ImagesViewSet(ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    search_fields = ["title", "description", "author"]
+    ordering_fields = ["title", "created_at"]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
