f63105b125fc425f2212ecdded56469efce882b6
Toma <greebsasha24@gmail.com>
2024-12-02 20:44:29 +0300

    Add: dir test for api app

---
+++ src/api/tests/__init__.py
@@ -0,0 +1,23 @@
+from api.views import CategoryModelViewSet, OrderModelViewSet, ProductModelViewSet
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+app_name = "api"
+
+
+router = routers.DefaultRouter()
+router.register(r"categories", CategoryModelViewSet)
+router.register(r"products", ProductModelViewSet)
+router.register(r"orders", OrderModelViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
