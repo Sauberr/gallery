6f2085f9a173df80ad2886fa5cac2d6e5253ea17
sauberr <sauberr10@gmail.com>
2025-03-31 20:44:23 +0300

    Add: commands for dev,prod settings

--- commands/start_server_dev.sh
+++ commands/start_server_dev.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 python src/manage.py migrate
+python src/manage.py loaddata fixtures/users.json
+python src/manage.py loaddata fixtures/subscriptions.json
+python src/manage.py loaddata fixtures/usersubscription.json
+python src/manage.py loaddata fixtures/images.json
 python src/manage.py runserver 0:8000
--- commands/start_server_prod.sh
+++ commands/start_server_prod.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 python src/manage.py migrate
+python src/manage.py loaddata fixtures/users.json
+python src/manage.py loaddata fixtures/subscriptions.json
+python src/manage.py loaddata fixtures/usersubscription.json
+python src/manage.py loaddata fixtures/images.json
 python src/manage.py collectstatic --noinput
 
