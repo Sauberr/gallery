3d36aef5989ee110f3f4ad721984efe5ad6b9c02
sauberr <sauberr10@gmail.com>
2025-07-13 02:18:52 +0300

    Add: view for feature-image.

--- src/images/views.py
+++ src/images/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls.base import reverse_lazy
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
 
 from common.mixins import CacheMixin, TitleMixin
 from core.utils.paginator import paginator
+from images.forms import ImageCreateForm
 from images.models import Images
 from subscriptions.models import UserSubscription
+from django.contrib import messages
 
 
 class ImagesList(LoginRequiredMixin, CacheMixin, TitleMixin, ListView):
@@ -79,3 +83,19 @@ class ImageDetail(LoginRequiredMixin, TitleMixin, DetailView):
         context["allowed_plans"] = allowed_plans
 
         return context
+
+
+class ImageCreateView(LoginRequiredMixin, TitleMixin, CreateView):
+    model = Images
+    form_class = ImageCreateForm
+    template_name: str = "partials/feature.html"
+    title: str = "Feature Image"
+    success_url = reverse_lazy("images:image-list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Image created successfully!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Please correct the errors below.')
+        return super().form_invalid(form)
