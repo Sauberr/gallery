a9551a8d4cbe036a863756be5c86a786b877cc00
sauberr <sauberr10@gmail.com>
2025-07-13 02:18:16 +0300

    Add: url for image-feature

--- src/images/urls.py
+++ src/images/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from images.views import ImageDetail, ImagesList
+from images.views import ImageDetail, ImagesList, ImageCreateView
 
 app_name: str = "images"
 
 urlpatterns = [
     path("images/", ImagesList.as_view(), name="images"),
     path("images/<int:pk>/", ImageDetail.as_view(), name="image_detail"),
+    path("feature/", ImageCreateView.as_view(), name="image_feature"),
 ]
